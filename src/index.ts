647e0e0d61f0c1101a260fe32b8c468adbe9aafb
xuyejian <nhxyj111@gmail.com>
2019-02-20 15:50:24 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { ApolloServer } from "apollo-server-express";
+import Express from "express";
+import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { createConnection } from "typeorm";
+import { RegisterResolver } from "./modules/user/Register";
+
+const main = async () => {
+  await createConnection();
+
+  const schema = await buildSchema({
+    resolvers: [RegisterResolver]
+  });
+
+  const apolloServer = new ApolloServer({ schema });
+
+  const app = Express();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log(`server started on http://localhost:4000`);
+  });
+};
+
+main();
---
+++ src/modules/user/Register.ts
@@ -0,0 +1,35 @@
+import * as bcrypt from "bcryptjs";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { User } from "../../entity/User";
+
+@Resolver()
+export class RegisterResolver {
+  @Query(() => String)
+  hello() {
+    return "hello";
+  }
+
+  // @FieldResolver()
+  // async name(@Root() parent: User) {
+  //   return `${parent.firstName} ${parent.lastName}`;
+  // }
+
+  @Mutation(() => User)
+  async register(
+    @Arg("firstName") firstName: string,
+    @Arg("lastName") lastName: string,
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword
+    }).save();
+
+    return user;
+  }
+}
